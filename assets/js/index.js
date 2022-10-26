68c8ce92de6dfaf170f4c9f3a04fa61b209e6368
Fred Awumey <fredawumeyfafa@gmail.com>
2022-10-26 18:50:53 +0000

    Updated files for new design

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+  // Dropzone has been added as a global variable.
+
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
